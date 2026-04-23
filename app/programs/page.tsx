2c3f65158934fce0a215238f812f38f78555331d
wanje ziro <samuelziro76@gmail.com>
2026-04-23 07:05:41 -0700

    fix eslint react scope error

--- app/programs/page.tsx
+++ app/programs/page.tsx
@@ -2,7 +2,7 @@
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-
+import React from "react";
 // ─── Types ───────────────────────────────────────────────────────────────────
 
 type Package = {
